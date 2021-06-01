82b7ebedd3c4ffa8fe78efa0111c913665e40dae
gedavarol <gizemeda91@gmail.com>
2021-06-01 21:44:07 +0200

    Transferring the project from Sandbox to GitHub

---
+++ script.js
@@ -0,0 +1,95 @@
+function formatDate(now) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherCondition(response) {
+  console.log(response);
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#feelsLike").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "eb31db6a7f8db876efe2238c26ce848c";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#form-city").value;
+  searchCity(city);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function searchLocation(position) {
+  let apiKey = "eb31db6a7f8db876efe2238c26ce848c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function clickFahrenheit(event) {
+  event.preventDefault();
+  let link = document.querySelector(".temperature");
+  link.innerHTML = "66";
+}
+
+function clickCelsius(event) {
+  event.preventDefault();
+  let link = document.querySelector(".temperature");
+  link.innerHTML = "19";
+}
+
+let currentTime = document.querySelector("h2");
+let now = new Date();
+currentTime.innerHTML = formatDate(now);
+
+let searchButton = document.querySelector("#search-form");
+searchButton.addEventListener("submit", showCity);
+
+let link = document.querySelector("#fahrenheit-link");
+link.addEventListener("click", clickFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", clickCelsius);
+
+let currentButton = document.querySelector(".currentButton");
+currentButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Barcelona");
