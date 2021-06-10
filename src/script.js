84fd8d0b29c08921046c5df869b8b70cc8fe125c
gedavarol <gizemeda91@gmail.com>
2021-06-10 15:57:41 +0200

    Forecast datas are displayed by API call

--- src/script.js
+++ src/script.js
@@ -20,23 +20,37 @@ function formatDate(now) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col">
-              <div class="forcast-day">${day}</div>
-              <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather-icon" width="42"></a>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col">
+              <div class="forcast-day">${formatDay(forecastDay.dt)}</div>
+              <img src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" alt="weather-icon" width="42"></a>
               <div class="forecast-temperatures">
-                <span class="forecast-temperature-max">18º</span>
-                <span class="forecast-temperature-min">12º</span>
+                <span class="forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}º</span>
+                <span class="forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}º</span>
               </div>
       </div>
           `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
